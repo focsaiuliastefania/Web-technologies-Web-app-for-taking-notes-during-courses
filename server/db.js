c4284e4364b3221c4a3f74283aae4bcb22c9e741
Focsa Iulia-Stefania <iuliastefania164@gmail.com>
2026-01-14 17:56:11 +0200

    update

--- server/db.js
+++ server/db.js
@@ -1,9 +1,22 @@
 const { Sequelize } = require('sequelize');
+require('dotenv').config();
 
-const sequelize = new Sequelize({
-  dialect: 'sqlite',
-  storage: './database.sqlite',
-  logging: false
-});
+const sequelize = process.env.DATABASE_URL
+  ? new Sequelize(process.env.DATABASE_URL, {
+      dialect: 'postgres',
+      protocol: 'postgres',
+      logging: false,
+      dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false
+        }
+      }
+    })
+  : new Sequelize({
+      dialect: 'sqlite',
+      storage: './database.sqlite',
+      logging: false
+    });
 
 module.exports = sequelize;
